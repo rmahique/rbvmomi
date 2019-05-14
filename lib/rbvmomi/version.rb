8862636ea4c3c67c76c56a69da0258dbc4563a23
J.R. Garcia <jrg@vmware.com>
2019-05-14 17:00:11 -0500

    Update version
    Updating version to begin work on next major release.
    
    Signed-off-by: J.R. Garcia <jrg@vmware.com>

--- lib/rbvmomi/version.rb
+++ lib/rbvmomi/version.rb
@@ -1,6 +1,6 @@
-# Copyright (c) 2016-2017 VMware, Inc.  All Rights Reserved.
+# Copyright (c) 2016-2019 VMware, Inc.  All Rights Reserved.
 # SPDX-License-Identifier: MIT
 
 module RbVmomi
-  VERSION = '2.1.2'.freeze
+  VERSION = '3.0.0.alpha'.freeze
 end
